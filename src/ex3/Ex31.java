dffbab4f3684d0d550dbd0858c04e42d5b6afef7
ronaldoxor <rronaldo.monteiro.almeida@gmail.com>
2026-02-19 11:14:51 -0300

    Exercício 3.1 de java

--- src/ex3/Ex31.java
+++ src/ex3/Ex31.java
@@ -1,4 +1,12 @@
 package ex3;
 
 public class Ex31 {
+    public static void main(String[] args){
+        int contador = 1;
+        while (contador < 11){
+            System.out.printf("%nO quadrado %d de é igual a: ", contador);
+            System.out.print(contador*contador);
+            contador += 1;
+        }
+    }
 }
